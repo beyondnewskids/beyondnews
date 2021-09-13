8884511abf6ba55e40763b89da1927685dc05d56
Perfect Squares <anuj01040916@gmail.com>
2021-09-13 20:29:40 +0530

    Hello World

---
+++ src/Contact.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                It's Me!
+            </div>
+            
+        );
+    }
+
+}
+
+export default Contact;
---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Hello World!
+            </div>
+            
+        );
+    }
+
+}
+
+export default Home;
