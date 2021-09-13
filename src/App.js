9c402a0f5b63e352736009101651dfdd58d490c2
Perfect Squares <anuj01040916@gmail.com>
2021-09-13 21:42:02 +0530

    Hello World

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 					  <Nav className="me-auto">
-						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="#">Home</Nav.Link>
 						<Nav.Link href="#about">About</Nav.Link>
 						<Nav.Link href="#contact">Contact</Nav.Link>
 						</Nav>
